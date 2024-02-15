edc65a1fa25d15684341024a9057c3fbb9cc0159
Diba Mirza <dimirza@cs.ucsb.edu>
2024-02-14 18:07:54 -0800

    Add files via upload

---
+++ main_set.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argv, char** argc){
+  if(argv < 3){
+    cout << "Please provide 2 file names" << endl;
+    return 1;
+  }
+  
+  ifstream cardFile1 (argc[1]);
+  ifstream cardFile2 (argc[2]);
+  string line;
+
+  if (cardFile1.fail() || cardFile2.fail() ){
+    cout << "Could not open file " << argc[2];
+    return 1;
+  }
+
+  //Read each file
+  while (getline (cardFile1, line) && (line.length() > 0)){
+
+  }
+  cardFile1.close();
+
+
+  while (getline (cardFile2, line) && (line.length() > 0)){
+
+  }
+  cardFile2.close();
+  
+  
+  return 0;
+}
