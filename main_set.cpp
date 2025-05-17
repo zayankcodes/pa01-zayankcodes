316f85cea336f9c93c753b0efd7cff8fc07a427c
Zayan <zayan@ucsb.edu>
2025-05-16 20:31:29 -0700

    1

--- main_set.cpp
+++ main_set.cpp
@@ -6,40 +6,37 @@
 #include <string>
 
 int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cout << "Please provide 2 file names\n";
+        return 1;
+    }
+
+    std::ifstream f1(argv[1]), f2(argv[2]);
+    if (f1.fail() || f2.fail()) {
+        std::cout << "Could not open file "
+                  << (f1.fail() ? argv[1] : argv[2])
+                  << "\n";
+        return 1;
+    }
+
     std::set<Card> alice, bob;
+    std::string line;
     char sc;
     std::string vs;
 
-    if (argc >= 3) {
-        std::ifstream f1(argv[1]), f2(argv[2]);
-        if (f1.fail() || f2.fail()) {
-            std::cout << "Could not open file "
-                      << (f1.fail() ? argv[1] : argv[2])
-                      << "\n";
-            return 1;
-        }
-        while (f1 >> sc >> vs) {
-            alice.insert(Card::fromChars(sc, vs));
-        }
-        while (f2 >> sc >> vs) {
-            bob.insert(Card::fromChars(sc, vs));
-        }
-    } else {
-        bool second = false;
-        std::string line;
-        while (std::getline(std::cin, line)) {
-            if (line.empty()) {
-                second = true;
-                continue;
-            }
-            std::istringstream iss(line);
-            iss >> sc >> vs;
-            if (!second)
-                alice.insert(Card::fromChars(sc, vs));
-            else
-                bob.insert(Card::fromChars(sc, vs));
-        }
+    while (std::getline(f1, line) && !line.empty()) {
+        std::istringstream iss(line);
+        iss >> sc >> vs;
+        alice.insert(Card::fromChars(sc, vs));
     }
+    f1.close();
+
+    while (std::getline(f2, line) && !line.empty()) {
+        std::istringstream iss(line);
+        iss >> sc >> vs;
+        bob.insert(Card::fromChars(sc, vs));
+    }
+    f2.close();
 
     while (true) {
         auto ait = alice.end();
