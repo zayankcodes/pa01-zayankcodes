3981804943eea8b47b065c384e604f39cb24972b
Zayan <zayan@ucsb.edu>
2025-05-16 20:15:31 -0700

    [a01

--- main_set.cpp
+++ main_set.cpp
@@ -1,50 +1,36 @@
 #include "card.h"
 #include <iostream>
-#include <fstream>
 #include <set>
 #include <sstream>
 #include <string>
 
-int main(int argc, char** argv) {
+int main() {
     std::set<Card> alice, bob;
+    std::string line;
+    bool second = false;
     char sc;
     std::string vs;
 
-    if (argc >= 3) {
-        std::ifstream f1(argv[1]), f2(argv[2]);
-        if (f1.fail() || f2.fail()) {
-            std::cout << "Could not open file "
-                      << (f1.fail() ? argv[1] : argv[2])
-                      << "\n";
-            return 1;
+    while (std::getline(std::cin, line)) {
+        if (line.empty()) {
+            second = true;
+            continue;
         }
-        while (f1 >> sc >> vs) {
+        std::istringstream iss(line);
+        iss >> sc >> vs;
+        if (!second)
             alice.insert(Card::fromChars(sc, vs));
-        }
-        while (f2 >> sc >> vs) {
+        else
             bob.insert(Card::fromChars(sc, vs));
-        }
-    } else {
-        bool second = false;
-        std::string line;
-        while (std::getline(std::cin, line)) {
-            if (line.empty()) {
-                second = true;
-                continue;
-            }
-            std::istringstream iss(line);
-            iss >> sc >> vs;
-            if (!second)
-                alice.insert(Card::fromChars(sc, vs));
-            else
-                bob.insert(Card::fromChars(sc, vs));
-        }
     }
 
     while (true) {
         auto ait = alice.end();
         for (auto it = alice.begin(); it != alice.end(); ++it) {
-            if (bob.count(*it)) { ait = it; break; }
+            if (bob.count(*it)) {
+                ait = it;
+                break;
+            }
         }
         if (ait == alice.end()) break;
         Card c = *ait;
@@ -54,7 +40,10 @@ int main(int argc, char** argv) {
 
         auto bit = bob.rend();
         for (auto it = bob.rbegin(); it != bob.rend(); --it) {
-            if (alice.count(*it)) { bit = it; break; }
+            if (alice.count(*it)) {
+                bit = it;
+                break;
+            }
         }
         if (bit == bob.rend()) break;
         c = *bit;
@@ -64,14 +53,9 @@ int main(int argc, char** argv) {
     }
 
     std::cout << "\nAlice's cards:\n";
-    for (auto& c : alice) {
-        std::cout << c.toString() << "\n";
-    }
-
+    for (auto& c : alice) std::cout << c.toString() << "\n";
     std::cout << "\nBob's cards:\n";
-    for (auto& c : bob) {
-        std::cout << c.toString() << "\n";
-    }
+    for (auto& c : bob)   std::cout << c.toString() << "\n";
 
     return 0;
 }
